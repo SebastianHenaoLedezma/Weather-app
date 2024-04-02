3a773a4eac0aa943f5dad8e44d460e7cd1267d32
Sebastian Henao <henao7730@gmail.com>
2024-04-02 17:07:38 -0500

    Revert "Conexion a la Api"
    This reverts commit 23b1c87ac1f5b204049c8831f3fdd3bcf1a4d52f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react'
-import config from './config/config';
-import axios from 'axios'
+import { useState } from 'react'
 import './App.css'
-import WeatherCard from './components/WeatherCard'
+import { useEffect } from 'react'
 
 function App() {
 
@@ -23,22 +21,12 @@ function App() {
     }
     navigator.geolocation.getCurrentPosition(success)
   }, [])
-
-
-  useEffect(() => {
-    if (coords) {
-      const APIKey = config.apiKey;
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&lang=es&appid=${APIKey}`
-
-      axios.get(url)
-        .then(res => setWeather(res.data))
-        .catch(err => console.log(err))
-    }
-  }, [coords])
+  
+  console.log(coords)
 
   return (
     <>
-      <WeatherCard weather={weather} />
+      <h1>Weather app</h1>
     </>
   )
 }
